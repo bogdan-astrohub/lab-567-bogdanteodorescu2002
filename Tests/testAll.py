e8ec001633b5ff924d50d8de1fcfaf8734a320fe
bogdan-astrohub <53422518+bogdan-astrohub@users.noreply.github.com>
2021-11-17 09:19:33 +0000

    Update testAll.py

--- Tests/testAll.py
+++ Tests/testAll.py
@@ -2,6 +2,7 @@ from Tests.testDomeniu import testCheltuiala
 from Tests.funct_tests import test_delete_all_costs_for_apartement, test_add_sum_to_date, \
     test_the_biggest_expense_for_every_type, test_ordering_expenses_descending_by_amount
 from Tests.crud_tests import test_create, test_read, test_update, test_delete
+from Tests.undo_redo_tests import test_undo_redo
 
 
 def runAllTests():
@@ -14,7 +15,4 @@ def runAllTests():
     test_add_sum_to_date()
     test_the_biggest_expense_for_every_type()
     test_ordering_expenses_descending_by_amount()
-
-    """
-    test_show_montly_amount_for_each_apartament()
-    """
+    test_undo_redo()
